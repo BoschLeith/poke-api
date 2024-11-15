d5412352ffd33bb9aa35f2b823daaa86add750d7
BoschLeith <bosch.leith@gmail.com>
2024-11-15 15:27:21 -0600

    feat: add CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
+import cors from "cors";
 import pokemonRoutes from "@routes/pokemon.routes";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/pokemon", pokemonRoutes);
 
